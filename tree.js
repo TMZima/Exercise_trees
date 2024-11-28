d6c9424495e542ee386f39b77e1eab87b08f0355
Trey Zima <trey.m.zima@gmail.com>
2024-11-28 11:38:59 -0700

    Completed sumValues function in tree.js

--- tree.js
+++ tree.js
@@ -14,22 +14,26 @@ class Tree {
 
   /** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
+  sumValues(node = this.root) {
+    // If there is not a node or is null/undefined, return 0.
+    if (!node) return 0;
+    let sum = node.val;
+    // Loop through the children of the node and add the sum of the children to the sum.
+    for (let child of node.children) {
+      // Recursively call sumValues on the children.
+      sum += this.sumValues(child);
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+  numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
